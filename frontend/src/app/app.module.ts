0a24f1428f21ca7450f97bd075d80b6137eb6a9b
Nipun <nipun.pra222@gmail.com>
2023-02-19 16:15:04 +0530

    Creation of partial component for order list.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { RegisterPageComponent } from './components/pages/register-page/register
 import { LoadingComponent } from './components/partials/loading/loading.component';
 import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
+import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
 
 @NgModule({
   declarations: [
@@ -45,6 +46,7 @@ import { CheckoutPageComponent } from './components/pages/checkout-page/checkout
     RegisterPageComponent,
     LoadingComponent,
     CheckoutPageComponent,
+    OrderItemsListComponent,
   ],
   imports: [
     BrowserModule,
